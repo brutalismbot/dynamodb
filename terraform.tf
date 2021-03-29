4f8ec469d84935499c93f970f542355eafc6b552
Alexander Mancevice <alexander.mancevice@hey.com>
2021-03-29 09:02:27 -0400

    major schema changes

--- terraform.tf
+++ terraform.tf
@@ -27,21 +27,16 @@ locals {
 }
 
 resource "aws_dynamodb_table" "brutalismbot" {
-  billing_mode   = "PROVISIONED"
-  hash_key       = "HASH"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "KEY"
   name           = "Brutalismbot"
-  range_key      = "SORT"
-  read_capacity  = 1
+  range_key      = null
+  read_capacity  = 0
   tags           = local.tags
-  write_capacity = 1
+  write_capacity = 0
 
   attribute {
-    name = "HASH"
-    type = "S"
-  }
-
-  attribute {
-    name = "SORT"
+    name = "KEY"
     type = "S"
   }
 
@@ -50,26 +45,64 @@ resource "aws_dynamodb_table" "brutalismbot" {
     type = "N"
   }
 
+  attribute {
+    name = "TYPE"
+    type = "S"
+  }
+
+  attribute {
+    name = "REDDIT_NAME"
+    type = "S"
+  }
+
+  attribute {
+    name = "SLACK_WEBHOOK"
+    type = "S"
+  }
+
+  attribute {
+    name = "TWITTER_APP"
+    type = "S"
+  }
+
   ttl {
     attribute_name = "TTL"
     enabled        = true
   }
 
   global_secondary_index {
-    name            = "CREATED_UTC"
-    hash_key        = "SORT"
+    name            = "CHRONO"
+    hash_key        = "TYPE"
     range_key       = "CREATED_UTC"
     projection_type = "KEYS_ONLY"
-    read_capacity   = 1
-    write_capacity  = 1
+    read_capacity   = 0
+    write_capacity  = 0
   }
 
   global_secondary_index {
-    name            = "LIST"
-    hash_key        = "SORT"
-    range_key       = "HASH"
+    name            = "REDDIT_NAMES"
+    hash_key        = "REDDIT_NAME"
+    range_key       = "KEY"
     projection_type = "ALL"
-    read_capacity   = 1
-    write_capacity  = 1
+    read_capacity   = 0
+    write_capacity  = 0
+  }
+
+  global_secondary_index {
+    name            = "SLACK_WEBHOOKS"
+    hash_key        = "SLACK_WEBHOOK"
+    range_key       = "KEY"
+    projection_type = "ALL"
+    read_capacity   = 0
+    write_capacity  = 0
+  }
+
+  global_secondary_index {
+    name            = "TWITTER_APPS"
+    hash_key        = "TWITTER_APP"
+    range_key       = "KEY"
+    projection_type = "ALL"
+    read_capacity   = 0
+    write_capacity  = 0
   }
 }
